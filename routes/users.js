03779ea31f7f69752ff84cebd1a5d8ae681083db
Darren Barklie <darrenbarklie@gmail.com>
2019-10-19 22:10:38 +0100

    Post login credentials to auth

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
 const config = require('config')
+
 const User = require('../models/User')
 
 // @route   POST api/users
