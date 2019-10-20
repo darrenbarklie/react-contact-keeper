d466c5b9d8892287a9cd31d0988f1199e3ff85e3
Darren Barklie <darrenbarklie@gmail.com>
2019-10-20 09:08:27 +0100

    Get all private contacts

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,23 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { check, validationResult } = require('express-validator')
+const auth = require('../middleware/auth')
 const config = require('config')
+const { check, validationResult } = require('express-validator')
 
 const User = require('../models/User')
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
 })
 
 // @route   POST api/auth
