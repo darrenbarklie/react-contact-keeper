07974a876bc0729f8b4576913c4e0cc3662ceba3
Darren Barklie <darrenbarklie@gmail.com>
2019-11-02 11:56:43 +0000

    Contact add and clear integration

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,20 +1,29 @@
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
+  CLEAR_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
 } from '../types'
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      }
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
+        loading: false,
       }
     case DELETE_CONTACT:
       return {
@@ -22,6 +31,7 @@ export default (state, action) => {
         contacts: state.contacts.filter(
           contact => contact.id !== action.payload
         ),
+        loading: false,
       }
     case SET_CURRENT:
       return {
@@ -48,7 +58,14 @@ export default (state, action) => {
           return contact.name.match(regex) || contact.email.match(regex)
         }),
       }
-
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        current: null,
+        error: null,
+      }
     case CLEAR_FILTER:
       return {
         ...state,
